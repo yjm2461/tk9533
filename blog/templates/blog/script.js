d748860797a1c2c6c4e403542a9294735cd7f23e
tk9533 <tk9533@naver.com>
2015-12-23 16:44:52 +0900

    Changed the HTML  for the site.

---
+++ blog/templates/blog/script.js
@@ -0,0 +1,10 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='example.html'>Example</a></li>" +
+"<li><a href='R.html'>What is the R?</a></li>" +
+"</ul>";
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " tk9533@koreatech.ac.kr. All rights reserved.</p>";
